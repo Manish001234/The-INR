58538491bcab8e134cfcfb3419c02b1e2ddc43c3
Manish Reddy <89566773+Manish001234@users.noreply.github.com>
2022-06-29 18:56:28 +0530

    random done

---
+++ ticket/src/components/Context.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const TicketContext = createContext();
+
+export const TicketContextProvider = ({ children }) => {
+    
+    const [Tickets,SetTickets] = useState([])
+
+   
+
+    return <TicketContext.Provider value={{ Tickets,SetTickets }}>
+            {children}
+    </TicketContext.Provider>
+} 
--- ticket/src/components/Ticket.jsx
+++ ticket/src/components/Ticket.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { TicketContext } from './Context'
 
 const Ticket = () => {
     const [Data, setData] = useState("")
+    
+    const {SetTickets,Tickets} = useContext(TicketContext)
+    // const [Tickets,SetTickets] = useContext(TicketContext)
 
+    // console.log(Tickets,"tickets")
+    // // Object.keys(Tickets).length=5
+    // Tickets.length=5
+      
     const handleTicket = (e)=>{
         const val = e.target.value;
         if(Data.length<=5) setData(Data+val)
         else alert("Enter only 6 numbers!")
          console.log(Data);
        }
-
+     
     const handleClear=()=>{
         setData("")
+      }
+
+      const randomTicket =()=>{
+        setData(Math.floor((Math.random()*1000000)+6))
+      
+        
       }
   return (
     <div className='container'>
@@ -32,18 +46,37 @@ const Ticket = () => {
           <button value="1" onClick={handleTicket}>1</button>
           <button value="2" onClick={handleTicket}>2</button>
           <button value="2" onClick={handleTicket}>3</button>
-          <button onClick={handleClear}>claer</button>
+          <button onClick={handleClear}>Clear</button>
           <button value="0" onClick={handleTicket}>0</button>
-          <button  onClick={handleClear}>delete</button>
+          <button  onClick={handleClear}>Delete</button>
          
        </div>
        <div >
-            <button id='addticket' >Add Ticket</button>
+            <button onClick={()=>{
+                SetTickets([...Tickets,Data])
+                setData("")
+                
+
+            }}  id='addticket' >+  Add Ticket</button>
           </div>
           </div>
        <div className='right'>
-             <img src="https://github.com/mugilmalathi/INR-Assignment/blob/main/inr-assignment/src/wheel.png?raw=true" alt="" height="100%" width="100%" />
+             <img onClick={randomTicket} src="https://github.com/mugilmalathi/INR-Assignment/blob/main/inr-assignment/src/wheel.png?raw=true" alt="" height="100%" width="100%" />
        </div>
+       </div>
+       <p className='p'>Your selected Tickets :</p>
+       <div className='box2'>
+        
+
+          {Tickets.map((e)=>{
+            return (
+                <div  >
+                     <div className='mapeddata'>{e}</div>
+              </div>
+            )
+          })}
+
+          
        </div>
     </div>
   )
